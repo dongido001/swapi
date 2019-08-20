b2f3827e42c86c30cbecc1c2f78bf13b207a7c90
Onwuka Gideon <dongidomed@gmail.com>
2019-08-20 18:08:57 +0100

    Add vue-slider and vue-router library

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
-  <div id="app">
-    ...
-  </div>
+  <b-container id="app" fluid>
+      <header class="header">
+        <img src="./assets/logo.png" width="85" height="25">
+        <SearchForm/>
+      </header>
+
+      <main>
+        <Starships/>
+        <Planets/>
+        <Characters/>
+      </main>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Characters from '@/components/Characters'
+import Planets from '@/components/Planets'
+import Starships from '@/components/Starships'
+import SearchForm from '@/components/SearchForm'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Characters,
+    Planets,
+    Starships,
+    SearchForm
   }
 }
 </script>
@@ -18,10 +33,40 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 0px;
+}
+
+.header {
+  height: 400px;
+  background-image: url("./assets/hero-banner.jpg");
+  padding: 10px;
+}
+
+.heading {
+  text-align: center;
+  padding-top: 35px;
+  font-weight: 300;
+  display: block;
+  padding-bottom: 5px;
+  position: relative;
+}
+
+.heading:before{
+    content: "";
+    position: absolute;
+    width: 5%;
+    bottom: 0;
+    left: 48%;
+    border-bottom: 3px solid gray;
+}
+
+.row-gap {
+  margin-bottom: 30px;
+}
+
+.container-width {
+    padding-right: 65px;
+    padding-left: 65px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueCarousel from 'vue-carousel'
+import VueRouter from 'vue-router'
 
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
+Vue.use(VueCarousel)
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
